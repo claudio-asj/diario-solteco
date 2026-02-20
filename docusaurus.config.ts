7b404e937a39cf0692d8996626ac8b70d213fd75
Claudio Jr <cla.junior21@gmail.com>
2026-02-20 17:47:05 -0300

    configurando deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,8 +23,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "claudio-asj", // Usually your GitHub org/user name.
+  projectName: "diario-solteco.github.io", // Usually your repo name.
+  deploymentBranch: "main",
 
   onBrokenLinks: "throw",
 
