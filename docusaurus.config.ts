45c02139688922833fb089fe97c598bd1001b37e
Claudio Jr <cla.junior21@gmail.com>
2026-02-20 17:48:55 -0300

    fix: deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://claudio-asj.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/diario-solteco/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
